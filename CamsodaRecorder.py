405e0ca80451b41953928ed77e9f62f64d17acb1
beaston02 <beaston02@gmail.com>
2017-10-20 17:53:36 -0700

    testing fix for issue #1
    testing fix for issue #1 
    
    Changed from urllib.requests to requests module

--- CamsodaRecorder.py
+++ CamsodaRecorder.py
@@ -1,5 +1,5 @@
 import sys, datetime, time, os, threading
-import urllib.request, json, random
+import requests, json, random
 from livestreamer import Livestreamer
 
 #specify path to save to ie "/Users/Joe/camsoda"
@@ -7,9 +7,14 @@ save_directory = "/Users/Joe/camsoda"
 #specify the path to the wishlist file ie "/Users/Joe/camsoda/wanted.txt"
 wishlist = "/Users/Joe/camsoda/wanted.txt"
 
-
+headers={
+    'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
+    'referer':'https://www.camsoda.com/'
+}
+r = requests.session()
+r.get('http://www.camsoda.com')
 def getOnlineModels():
-    num = random.randint(1000, 9999)
+    num = random.randint(1000, 30000)
     wanted = []
     with open(wishlist) as f:
         for line in f:
@@ -18,8 +23,8 @@ def getOnlineModels():
                 wanted.append(theModel.lower())
     f.close()
     url = "https://www.camsoda.com/api/v1/browse/online"
-    with urllib.request.urlopen(url) as url:
-        data = json.loads(url.read().decode())
+    with r.get(url, headers=headers) as url:
+        data = url.json()
         for line in data['results']:
             if line['username'].lower() not in recording and line['status'] != 'connected':
                 if line['display_name'].lower() in wanted or line['username'].lower() in wanted:
@@ -33,35 +38,35 @@ def getOnlineModels():
 def startRecording(modelData, num):
     try:
         recording.append(modelData['username'].lower())
-        with urllib.request.urlopen(
-                "https://www.camsoda.com/api/v1/video/vtoken/{model}?username=guest_{num}".format(model=modelData['username'], num=num)) as videoapi:
-            data2 = json.loads(videoapi.read().decode())
-            if str(modelData['status']) != 'limited':
-                if str(modelData['status']) == 'private':
-                    server = data2['private_servers'][0]
-                elif modelData['status'] == 'online':
-                    server = data2['edge_servers'][0]
-                else:
-                    server = data2['mjpeg_server']
-                link = "hlsvariant://https://{server}/{app}/mp4:{stream_name}_mjpeg/playlist.m3u8?token={token}".format(server=server, app=data2['app'], stream_name=data2['stream_name'], token=data2['token'])
-            if not os.path.exists("{path}/{model}".format(path=save_directory, model=modelData['username'])):
-                os.makedirs("{path}/{model}".format(path=save_directory, model=modelData['username']))
-            session = Livestreamer()
-            streams = session.streams(link)
-            stream = streams["best"]
-            fd = stream.open()
-            ts = time.time()
-            st = datetime.datetime.fromtimestamp(ts).strftime("%Y.%m.%d_%H.%M.%S")
-            with open("{path}/{model}/{st}_{model}.mp4".format(path=save_directory, model=modelData['username'], st=st), 'wb') as f:
-                while True:
-                    try:
-                        data = fd.read(1024)
-                        f.write(data)
-                    except:
-                        recording.remove(modelData['username'].lower())
-                        #print("{} stream has ended".format(modelData['username']))
-                        f.close()
-                        return()
+        videoapi = requests.get(
+                "https://www.camsoda.com/api/v1/video/vtoken/{model}?username=guest_{num}".format(model=modelData['username'], num=num), headers=headers)
+        data2 = json.loads(videoapi.text)
+        if str(modelData['status']) != 'limited':
+            if str(modelData['status']) == 'private':
+                server = data2['private_servers'][0]
+            elif modelData['status'] == 'online':
+                server = data2['edge_servers'][0]
+            else:
+                server = data2['mjpeg_server']
+            link = "hlsvariant://https://{server}/{app}/mp4:{stream_name}_mjpeg/playlist.m3u8?token={token}".format(server=server, app=data2['app'], stream_name=data2['stream_name'], token=data2['token'])
+        if not os.path.exists("{path}/{model}".format(path=save_directory, model=modelData['username'])):
+            os.makedirs("{path}/{model}".format(path=save_directory, model=modelData['username']))
+        session = Livestreamer()
+        streams = session.streams(link)
+        stream = streams["best"]
+        fd = stream.open()
+        ts = time.time()
+        st = datetime.datetime.fromtimestamp(ts).strftime("%Y.%m.%d_%H.%M.%S")
+        with open("{path}/{model}/{st}_{model}.mp4".format(path=save_directory, model=modelData['username'], st=st), 'wb') as f:
+            while True:
+                try:
+                    data = fd.read(1024)
+                    f.write(data)
+                except:
+                    recording.remove(modelData['username'].lower())
+                    #print("{} stream has ended".format(modelData['username']))
+                    f.close()
+                    return()
         recording.remove(modelData['username'].lower())
         #print("{} stream has ended".format(modelData['username']))
 
